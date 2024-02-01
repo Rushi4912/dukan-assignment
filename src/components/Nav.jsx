069dbad8e81511f3ab4239fc6eebd0201d2ac403
Rushi4912 <pawarrushikeshpawar@gmail.com>
2024-02-01 17:50:39 +0530

    change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,11 +22,12 @@ const Nav = () => {
           <MessageSquareMore  className="w-[2rem] m-1"/>
         </div>
         <div className="w-[2.5rem] h-[2.5rem] border rounded-full bg-gray-200">
-          <ChevronDownCircle className="w-[2rem] m-1" />
+          <ChevronDownCircle className="w-[2rem] " />
         </div>
       </div>
     </div>
   );
 };
 
-export default Nav;
+export default Nav;;
+
